e9b4195c514f191bd0cc967c44045858da3a70e9
Pedro Henrique <pedrohenriquedapaixao@outlook.com>
2021-07-28 16:09:03 -0300

    Add project files.

---
+++ CursoDIO-ASPMVC/Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CursoDIO_ASPMVC.Models
+{
+    public class Context : IdentityDbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+        }*/
+    }
+}
---
+++ CursoDIO-ASPMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CursoDIO_ASPMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
